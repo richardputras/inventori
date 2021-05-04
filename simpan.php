5ab3f9fc157f150c72bc8a4564749ea81ee48a35
richardputras <71116856+richardputras@users.noreply.github.com>
2021-05-04 11:16:27 +0700

    Delete simpan.php

--- simpan.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$data_uri = $_POST['foto'];
-$encoded_image = explode(",", $data_uri)[1];
-$decoded_image = base64_decode($encoded_image);
-file_put_contents("signature.png", $decoded_image);
